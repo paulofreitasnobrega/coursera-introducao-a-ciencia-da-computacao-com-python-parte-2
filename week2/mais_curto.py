8f3ba1c3352c883c95cdc2c3a23193e34a2444c8
paulofreitasnobrega <paulofreitasnobrega@gmail.com>
2019-09-18 21:08:32 -0300

    Semana 2 - Exercícios - Ajustes no exercício Nome Mais Curto

--- week2/mais_curto.py
+++ week2/mais_curto.py
@@ -7,32 +7,65 @@
 
 #Aluno: Paulo Freitas Nobrega
 
-# Recebe uma lista de nome e retorna o menor deles capitalizado
+# Recebe uma lista de nomes e retorna o menor deles capitalizado. Se houver
+# nomes com o mesmo comprimento, ordena-os alfanumericamente e retorna o
+# primeiro resultado encontrado. Se uma lista vazia for passada como parâmetro
+# ou qualquer valor diferente de lista, retorna "Anônimo".
 def nomeMaisCurto(nomes):
-	menorNome = nomes[0]
-	menorNumeroDeCaracteres = len(nomes[0])
+	# se não receber uma lista de nomes valida, retorne "Anônimo"
+	if not len(nomes) or type(nomes) != list:
+		return "Anônimo"
+
+	# inicia o nome curto com o primeiro nome da lista recebida
+	nomeCurto = nomes[0]
+	comprimentoDoNomeCurto = len(nomes[0])
 
 	for nome in nomes:
+		# quando houver espaços em branco, remova-os
 		nome = nome.strip()
-		if len(nome) < menorNumeroDeCaracteres:
-			menorNumeroDeCaracteres = len(nome)
-			menorNome = nome
 
-	return menorNome.capitalize()
+		# se encontrar um nome menor que o nome definido como mais curto,
+		# substitua-o
+		if len(nome) < comprimentoDoNomeCurto:
+			comprimentoDoNomeCurto = len(nome)
+			nomeCurto = nome
 
-# Realiza os testes
+		# se houver nomes com o mesmos comprimentos, ordene-os alfanumericamente
+		# e defina como mais curto o primeiro resultado
+		elif len(nome) == comprimentoDoNomeCurto and nomeCurto != nome:
+			listaDeComparacao = sorted([nomeCurto, nome])
+			nomeCurto = listaDeComparacao[0]
+
+	# retorne o nome mais curto capitalizado
+	return nomeCurto.capitalize()
+
+
+# Testa o nome mais curto
 def test_nomeMaisCurto():
-	nome1 = ["ana", "maria", "francisco"]
-	nome2 = ["    ana     ", "maria", "francisco"]
-	nome3 = ["aNA", "maria", "francisco"]
+	nomes = ["ana", "maria", "francisco"]
+	assert nomeMaisCurto(nomes) == "Ana"
 
-	nomes = [nome1, nome2, nome3]
+# Testa nomes com espaços em branco
+def test_nomesComEspacos():
+	nomes = ["    ana     ", "maria", "francisco"]
+	assert nomeMaisCurto(nomes) == "Ana"
 
-	for n in nomes:
-		nome = nomeMaisCurto(n)
-		if nome != "Ana" or len(nome) != 3:
-			return False
+# Testa nomes com letras maiúsculas e minúsculas
+def test_nomesComMaiusculasEMinusculas():
+	nomes = ["aNA", "maria", "francisco"]
+	assert nomeMaisCurto(nomes) == "Ana"
 
-	return True
+# Testa nomes com mesmo comprimento
+def test_nomesComOMesmoComprimento():
+	nomes = ["paulo", "maria", "francisco"]
+	assert nomeMaisCurto(nomes) == "Maria"
 
-print(test_nomeMaisCurto())
+# Testa lista de nomes vazia
+def test_listaDeNomesVazia():
+	nomes = []
+	assert nomeMaisCurto(nomes) == "Anônimo"
+
+# Testa lista inválida
+def test_listaDeNomesInvalida():
+	nomes = "carlos"
+	assert nomeMaisCurto(nomes) == "Anônimo"
