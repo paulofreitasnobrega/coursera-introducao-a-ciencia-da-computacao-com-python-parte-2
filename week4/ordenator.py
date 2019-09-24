b509c80ce4c90b768804f1742258e16354039a42
Paulo Freitas Nobrega <paulofreitasnobrega@gmail.com>
2019-09-24 17:17:23 -0300

    Semana 04 - Exercícios - Parte I

---
+++ week4/ordenator.py
@@ -0,0 +1,24 @@
+"""Week 4 - Exercício em vídeo - Ordenador de Seleção Direta."""
+
+
+class Ordenator:
+    """Possui métodos de ordenação de elementos."""
+
+    def __init__(self, list: list):
+        """Método construtor."""
+        self.list = list
+
+    def direct_selection(self) -> list:
+        """Ordena em forma crescente uma lista de números."""
+        list = self.list[:]
+
+        for i in range(len(list)-1):
+            min_position = i
+
+            for j in range(i+1, len(list)):
+                if list[j] < list[min_position]:
+                    min_position = j
+
+            list[i], list[min_position] = list[min_position], list[i]
+
+        return list
